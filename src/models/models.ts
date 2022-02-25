9c6825a28a10124bd9b3eefb83cb5fcb288a5105
ljgleeson <ljgleeson2106@gmail.com>
2022-02-25 09:32:40 -0500

    cleaned up code

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,5 +1,3 @@
-//In-Memory Storage 
-
 import { DataTypes } from 'sequelize/types';
 import { v4 as uuid } from 'uuid';
 
@@ -30,18 +28,8 @@ export const setTasks = [
 
 
 
-//"a model should export a function that returns a Sequelize model defintion"
-/*
-export default function (sequelize, Datatypes) {
-    const Category = sequelize.define('Category', {
-        id: DataTypes.UUID,
-        createdAt: DataTypes.DATE,
-        title: DataTypes.STRING,
-        dueDate: DataTypes.DATE,
-        completed: DataTypes.BOOLEAN
-    })
-}
-*/
+
+
 
 /*
 function DB(sequelize, models) {
@@ -49,8 +37,6 @@ function DB(sequelize, models) {
     this.models = models
 }
 server.plugins['hapi-sequelizejs'][opts.name] = new DB(opts.sequelize, models);
-
-
 */
 
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,5 @@
 import { v4 as uuid } from 'uuid';
 import { task_Repo, Task } from "../repo/iRepository"
-import { Request, ResponseToolkit } from "@hapi/hapi"
 
 export const routes = [
     {  
